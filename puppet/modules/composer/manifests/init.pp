d54300b619bbebdfe9775fc4e09f8af855aa870f
Christopher Lamm <chris@theantichris.com>
2014-09-03 11:06:56 -0400

    Composer module now runs an update.

--- puppet/modules/composer/manifests/init.pp
+++ puppet/modules/composer/manifests/init.pp
@@ -1,9 +1,9 @@
-class composer {
+class composer($workingDir = '/vagrant', $composerHome='/home/vagrant/.composer') {
   exec {
     'download-composer':
     command => 'curl -sS https://getcomposer.org/installer | php',
     unless => 'ls /usr/local/bin/composer',
-    require => Package['curl', 'php5-cli']
+    require => Package['curl', 'php5-cli'],
   }
 
   # Move Composer to be accessed globally.
@@ -11,6 +11,16 @@ class composer {
     'move-composer':
     command => 'mv composer.phar /usr/local/bin/composer',
     unless => 'ls /usr/local/bin/composer',
-    require => Exec['download-composer']
+    require => Exec['download-composer'],
+  }
+
+  # Run composer update if a composer.json file exists.
+  exec {
+    'composer-update':
+    command => 'composer update',
+    cwd => "${workingDir}",
+    environment => "COMPOSER_HOME=${composerHome}",
+    onlyif => "ls ${$workingDir}/composer.json",
+    require => Exec['move-composer'],
   }
 }
