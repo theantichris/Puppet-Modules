04dfd2f7272601856f598dc1b7e05a7792992820
Christopher Lamm <chris@theantichris.com>
2014-09-25 18:04:59 -0400

    mcrypt needs to be enabled manually.

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,13 +1,20 @@
 class php($webRoot = '/vagrant/web') {
   package {
     ['php5-fpm', 'php5-cli', 'php5-mysql', 'php5-curl', 'php5-mcrypt']:
-    ensure => present,
-    require => Exec['update-package-list'],
+      ensure  => present,
+      require => Exec['update-package-list'],
   }
 
   service {
     'php5-fpm':
-    ensure => running,
-    require => Package['php5-fpm'],
+      ensure  => running,
+      require => Package['php5-fpm'],
+  }
+
+  exec {
+    'enable-mcrypt':
+      command => 'sudo php5enmod mcrypt',
+      require => Package['php5-mcrypt'],
+      notify  => Service['php5-fpm']
   }
 }
