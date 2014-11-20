f18edd7f476e24d921e66a9cf9a700e39ccad43d
Christopher Lamm <chris@theantichris.com>
2014-11-20 13:28:56 -0500

    Added execution of bower install.

--- puppet/modules/bower/manifests/init.pp
+++ puppet/modules/bower/manifests/init.pp
@@ -1,8 +1,17 @@
-class bower {
+class bower($cwd = '/vagrant') {
   exec {
     'install-bower':
-    command => 'sudo npm install -g bower',
-    unless => 'ls /usr/bin/bower',
-    require => Package['nodejs'],
+      command => 'sudo npm install -g bower',
+      unless  => 'ls /usr/bin/bower',
+      require => Package['nodejs'],
+  }
+
+  exec {
+    'bower-install':
+      command => 'bower install --quiet --config.interactive=false',
+      cwd     => "${cwd}",
+      user    => 'vagrant',
+      onlyif  => "ls ${cwd}/bower.json",
+      require => Exec['install-bower'],
   }
 }
