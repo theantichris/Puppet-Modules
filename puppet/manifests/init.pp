2fdff243267aba6fb90f5d06a7907ad6a1f3dfd0
Christopher Lamm <chris@theantichris.com>
2014-11-22 09:37:47 -0500

    Updated MongoDB module.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -12,4 +12,4 @@ host {
 }
 
 # include <comma separated list of desired modules>
-include nodejs, bower, gruntjs
+include mongodb
--- puppet/modules/mongodb/manifests/init.pp
+++ puppet/modules/mongodb/manifests/init.pp
@@ -1,22 +1,22 @@
 class mongodb {
   exec {
-    'import-mongodb-public-key':
+    'import mongodb public key':
       command => 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10',
       unless  => "apt-key list | grep 7F0CEB10",
+      require => Exec['install packages'],
   }
 
   exec {
-    'add-mongodb-repo':
-      command => "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list",
-      unless  => 'ls /etc/apt/sources.list.d/mongodb.list',
-      notify  => Exec['update-for-repo'],
-      require => Package['python-software-properties', 'python', 'g++', 'make'],
+    'add mongodb repo':
+      command  => "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list && sudo apt-get update",
+      creates  => '/etc/apt/sources.list.d/mongodb.list',
+      require  => Exec['import mongodb public key'],
   }
 
   package {
     ['mongodb-org', 'php5-mongo']:
       ensure  => present,
-      require => Exec['add-mongodb-repo', 'update-for-repo'],
+      require => Exec['add mongodb repo'],
   }
 
   service {
