1e09e85cb1dcc42ddf3c8202d2a5e3b2b413c339
Christopher Lamm <chris@theantichris.com>
2014-11-22 09:33:36 -0500

    Formatting.

--- puppet/modules/mongodb/manifests/init.pp
+++ puppet/modules/mongodb/manifests/init.pp
@@ -1,27 +1,27 @@
 class mongodb {
   exec {
     'import-mongodb-public-key':
-    command => 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10',
-    unless => "apt-key list | grep 7F0CEB10",
+      command => 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10',
+      unless  => "apt-key list | grep 7F0CEB10",
   }
 
   exec {
     'add-mongodb-repo':
-    command => "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list",
-    unless => 'ls /etc/apt/sources.list.d/mongodb.list',
-    notify => Exec['update-for-repo'],
-    require => Package['python-software-properties', 'python', 'g++', 'make'],
+      command => "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list",
+      unless  => 'ls /etc/apt/sources.list.d/mongodb.list',
+      notify  => Exec['update-for-repo'],
+      require => Package['python-software-properties', 'python', 'g++', 'make'],
   }
 
   package {
     ['mongodb-org', 'php5-mongo']:
-    ensure => present,
-    require => Exec['add-mongodb-repo', 'update-for-repo'],
+      ensure  => present,
+      require => Exec['add-mongodb-repo', 'update-for-repo'],
   }
 
   service {
     'mongod':
-    ensure => running,
-    require => Package['mongodb-org']
+      ensure  => running,
+      require => Package['mongodb-org']
   }
 }
