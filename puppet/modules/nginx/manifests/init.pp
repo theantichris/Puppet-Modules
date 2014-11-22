4970c688e8be758616cb38eb974c1c231309dc1b
Christopher Lamm <chris@theantichris.com>
2014-11-22 09:45:15 -0500

    Updated Nginx module.

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,58 +1,57 @@
 class nginx($webRoot = '/vagrant/web') {
   exec {
-    'download-nginx-key':
-    command => 'wget http://nginx.org/keys/nginx_signing.key',
-    unless => 'grep -rl "deb http://nginx.org/packages/ubuntu/ trusty nginx" /etc/apt/sources.list',
-    require => Package['python-software-properties'],
+    'download nginx key':
+      command => 'wget http://nginx.org/keys/nginx_signing.key',
+      unless  => 'grep -rl "deb http://nginx.org/packages/ubuntu/ trusty nginx" /etc/apt/sources.list',
+      require => Exec['install packages'],
+      notify  => Exec['add nginx key'],
   }
 
   exec {
-    'add-nginx-key':
-    command => 'sudo apt-key add nginx_signing.key',
-    unless => 'grep -rl "deb http://nginx.org/packages/ubuntu/ trusty nginx" /etc/apt/sources.list',
-    require => Exec['download-nginx-key']
+    'add nginx key':
+      command     => 'sudo apt-key add nginx_signing.key',
+      refreshonly => true,
+      notify      => Exec['add nginx repo'],
   }
 
   exec {
-    'add-nginx-repo':
-    command => 'sudo add-apt-repository "deb http://nginx.org/packages/ubuntu/ trusty nginx"',
-    unless => 'grep -rl "deb http://nginx.org/packages/ubuntu/ trusty nginx" /etc/apt/sources.list',
-    require => Exec['add-nginx-key'],
-    notify => Exec['update-for-repo']
+    'add nginx repo':
+      command     => 'sudo add-apt-repository "deb http://nginx.org/packages/ubuntu/ trusty nginx" && sudo apt-get update',
+      refreshonly => true,
   }
 
   package {
     'nginx':
-    ensure => present,
-    require => Exec['add-nginx-repo', 'update-for-repo'],
+      ensure  => present,
+      require => Exec['add nginx repo'],
   }
 
   service {
     'nginx':
-    ensure => running,
-    require => Package['nginx'],
+      ensure  => running,
+      require => Package['nginx'],
   }
 
   exec {
-    'add-nginx-to-www-data':
-    command => 'sudo usermod -a -G www-data nginx',
-    unless => 'groups nginx | grep "www-data"',
-    notify => Service['nginx'],
-    require => Package['nginx'],
+    'add nginx to www-data':
+      command => 'sudo usermod -a -G www-data nginx',
+      unless  => 'groups nginx | grep "www-data"',
+      notify  => Service['nginx'],
+      require => Package['nginx'],
   }
 
-  # Create the web root folder.
+# Create the web root folder.
   file {
     "${webRoot}":
-    ensure => directory,
+      ensure => directory,
   }
 
-  # Create the virtual host file from the module.
+# Create the virtual host file from the module.
   file {
     '/etc/nginx/conf.d/puppet.conf':
-    ensure => present,
-    source => 'puppet:///modules/nginx/puppet.conf',
-    notify => Service['nginx'],
-    require => Package['nginx'],
+      ensure  => present,
+      source  => 'puppet:///modules/nginx/puppet.conf',
+      notify  => Service['nginx'],
+      require => Package['nginx'],
   }
 }
