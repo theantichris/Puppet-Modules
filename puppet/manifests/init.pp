c64a6b469704d75117ec2ab8477575dd166fbc7b
Christopher Lamm <chris@theantichris.com>
2014-11-22 09:58:42 -0500

    Updated Composer module.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -12,4 +12,4 @@ host {
 }
 
 # include <comma separated list of desired modules>
-include nginx, php
+include nginx, php, composer
--- puppet/modules/composer/manifests/init.pp
+++ puppet/modules/composer/manifests/init.pp
@@ -1,16 +1,24 @@
-class composer() {
+class composer($cwd = '/vagrant', $user = 'vagrant') {
   exec {
-    'download-composer':
-    command => 'curl -sS https://getcomposer.org/installer | php',
-    unless => 'ls /usr/local/bin/composer',
-    require => Package['curl', 'php5-cli'],
+    'download composer':
+      command => 'curl -sS https://getcomposer.org/installer | php',
+      creates => '/usr/local/bin/composer',
+      require => Package['php5-cli'],
+      notify  => Exec['move composer'],
   }
 
-  # Move Composer to be accessed globally.
   exec {
-    'move-composer':
-    command => 'mv composer.phar /usr/local/bin/composer',
-    unless => 'ls /usr/local/bin/composer',
-    require => Exec['download-composer'],
+    'move composer':
+      command     => 'mv composer.phar /usr/local/bin/composer',
+      refreshonly => true,
+  }
+
+  exec {
+    'run composer update':
+      command => 'composer update --quiet --no-interaction',
+      cwd     => $cwd,
+      user    => $user,
+      onlyif  => "ls ${cwd}/composer.json",
+      require => Exec['move composer'],
   }
 }
