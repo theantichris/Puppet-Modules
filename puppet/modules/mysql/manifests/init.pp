9fd4e9e9d01b5e82b3fe215d1184fae39e013eb5
Christopher Lamm <chris@theantichris.com>
2014-11-22 10:18:59 -0500

    Formatting.

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -1,46 +1,46 @@
 class mysql($rootUsername = 'root', $rootPassword = '123', $databaseName = 'database_dev') {
   file {
     '/etc/mysql':
-    ensure => directory,
+      ensure => directory,
   }
 
   file {
     '/etc/mysql/my.cnf':
-    ensure => present,
-    source => 'puppet:///modules/mysql/my.cnf',
-    require => File['/etc/mysql'],
+      ensure  => present,
+      source  => 'puppet:///modules/mysql/my.cnf',
+      require => File['/etc/mysql'],
   }
 
   exec{
     'set-myconf-permissions':
-    command => 'sudo chmod 644 /etc/mysql/my.cnf',
-    require => File['/etc/mysql/my.cnf'],
+      command => 'sudo chmod 644 /etc/mysql/my.cnf',
+      require => File['/etc/mysql/my.cnf'],
   }
 
   package {
     ['mysql-server-5.6', 'php5-mysql']:
-    ensure => present,
-    require => Exec['update-package-list', 'set-myconf-permissions'],
+      ensure  => present,
+      require => Exec['update-package-list', 'set-myconf-permissions'],
   }
 
   service {
     'mysql':
-    ensure => running,
-    require => Package['mysql-server-5.6']
+      ensure  => running,
+      require => Package['mysql-server-5.6']
   }
 
   exec {
     'set-mysql-root-password':
-    unless  => "mysqladmin -u${rootUsername} -p${rootPassword} status",
-    command => "mysqladmin -u ${rootUsername} password ${rootPassword}",
-    require => Service['mysql']
+      unless  => "mysqladmin -u${rootUsername} -p${rootPassword} status",
+      command => "mysqladmin -u ${rootUsername} password ${rootPassword}",
+      require => Service['mysql']
   }
 
-  # Create a new database.
+# Create a new database.
   exec {
     'create-database':
-    unless => "mysql -u${rootUsername} -p${rootPassword} ${databaseName}",
-    command => "mysql -u${rootUsername} -p${rootPassword} -e \"create database ${databaseName};\"",
-    require => Exec['set-mysql-root-password']
+      unless  => "mysql -u${rootUsername} -p${rootPassword} ${databaseName}",
+      command => "mysql -u${rootUsername} -p${rootPassword} -e \"create database ${databaseName};\"",
+      require => Exec['set-mysql-root-password']
   }
 }
